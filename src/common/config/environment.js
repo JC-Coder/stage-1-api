02f9e0ab1c5f98ca1b4814af91e54405da5a7236
JC-Coder <josephchimezie2003@gmail.com>
2024-06-30 23:54:25 +0100

    update

--- src/common/config/environment.js
+++ src/common/config/environment.js
@@ -7,4 +7,7 @@ export const ENVIRONMENT = {
         PORT: process.env.PORT || 3000,
         ENV: process.env.APP_ENV,
     },
+    OPEN_WEATHER: {
+        API_KEY: process.env.OPEN_WEATHER_API_KEY,
+    },
 }
